243af191df4c69700d24fe825a862afb9b0b2339
jmajrs <jmajrs@gmail.com>
2022-10-08 19:11:30 -0500

    Starting conditions

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// Proyecr #3 Pig Game
+
+// Selecting elements
+const score0El = document.querySelector('#score--0'); // Esta es una forma básica de seleccionar un elemento
+const score1El = document.getElementById('score--1'); // Con esta forma lo seleccionamos por su ID directamente
+const diceEl = document.querySelector('.dice');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+
